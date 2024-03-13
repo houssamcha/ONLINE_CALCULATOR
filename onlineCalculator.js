6e99224db5022774f4589521b6f78e4b2c7477f2
houssamcha <chagourihoussam@gmail.com>
2024-03-13 15:22:25 +0000

    Creation de la function Calculate en JS ; qui fait le logique des calcules

--- onlineCalculator.js
+++ onlineCalculator.js
@@ -6,3 +6,11 @@ function appendToDisplay(input){
 function clearDisplay(){
     display.value = "";
 }
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "ERROR!";
+    }
+}
